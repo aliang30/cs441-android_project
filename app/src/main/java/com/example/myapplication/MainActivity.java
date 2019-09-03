42785c4229aa71b9b410044e5facec4f2a531f58
aliang30 <aliang30@binghamton>
2019-09-02 23:05:57 -0400

    Modified layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,6 +14,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,4 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/UnitConverter.java
+++ app/src/main/java/com/example/myapplication/UnitConverter.java
@@ -1,4 +1,12 @@
 package com.example.myapplication;
 
 public class UnitConverter {
+    public static double celsius2Farenheit(double f) {
+        return (f - 32) * 5 / 9;
+    }
+
+    public static double farenheit2Celcius(double c) {
+        return 32 + c * 9 / 5;
+    }
+
 }
